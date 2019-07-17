4246514f90628f674234917cdeb79eff851488b9
yoyoyo11 <johann@lidlabinc.com>
2019-07-17 10:43:38 -0700

    customized Slider's theme by defining SliderTheme using SliderTheme.of(context).copyWith(...)

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -88,18 +88,29 @@ class _InputPageState extends State<InputPage> {
                       )
                     ],
                   ),
-                  Slider(
-                    onChanged: (double newValue) {
-                      setState(() {
-                        height = newValue.round();
-                      });
-                    },
-                    value: height.toDouble(),
-                    min: 120.0,
-                    max: 220.0,
-                    activeColor: Color(0xFFEB1555),
-                    inactiveColor: Color(0xFF8D8E98),
-                  )
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      activeTrackColor: Colors.white,
+                      inactiveTrackColor: Color(0xFF8D8E98),
+                      thumbColor: Color(0xFFEB1555),
+                      overlayColor:
+                          Color(0x29EB1555), //note that 0x29 is 16% opacity
+                      thumbShape:
+                          RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                      overlayShape:
+                          RoundSliderOverlayShape(overlayRadius: 30.0),
+                    ),
+                    child: Slider(
+                      onChanged: (double newValue) {
+                        setState(() {
+                          height = newValue.round();
+                        });
+                      },
+                      value: height.toDouble(),
+                      min: 120.0,
+                      max: 220.0,
+                    ),
+                  ),
                 ],
               ),
             ),
